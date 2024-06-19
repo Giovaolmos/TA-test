90336d092d820e214da074f1cfde2dfbd8fa22f9
Giovaolmos <127804984+Giovaolmos@users.noreply.github.com>
2024-06-19 16:29:17 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import express from "express"
+import "dotenv/config"
+import axios from "axios"
+
+
+const PORT = process.env.PORT || 5147
+const URL_API = process.env.URL_API || "https://ajoaquinlizarraga.github.io/apipublic/mydata/people/people.json"
+
+
+export const server = express();
+server.use(express.json())
+
+server.listen(PORT, () => {
+    console.log(`Server escuchando en puerto ${PORT}`)
+})
+
+server.get("/soyTA", async(req, res) => {
+    try {
+        const response = await axios(URL_API);
+        res.status(200).json(response.data)
+    } catch (error) {
+        res.status(500).json({error:"Algo salió mal"})
+    }
+})
+
+
+
+
+
